10a9870dc6d6692d63c375cd866dad25572ba529
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-28 14:50:07 -0400

    Configured Vite HMR and watch

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
+            input: [
+                "resources/js/app.tsx",
+                "resources/css/app.css",
+                // additional files to input
+            ],
+            ssr: "resources/js/ssr.tsx",
+            refresh: [
+                "resources/js/**/*",
+                "resources/css/**/*",
+                // additional folders to watch for refresh
+            ],
         }),
         react(),
     ],
+    server: {
+        host: "laravel11reactspa.thecodeholic.test",
+        port: 8084,
+        hmr: {
+            host: "laravel11reactspa.thecodeholic.test",
+            port: 8084,
+        },
+    },
 });
