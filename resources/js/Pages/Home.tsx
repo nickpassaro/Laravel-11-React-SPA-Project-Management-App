efbc97ce33d960769832034e6436cb25938efb93
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-28 23:32:59 -0400

    Ripped nav bar out of authenticated layout and made the nav bar a reusable component instead

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,7 +1,13 @@
-const Home = () => {
-  return (
-    <h1>Hello World</h1>
-  )
-}
+import NavBar from "@/Components/NavBar";
 
-export default Home
+const Home = () => {
+    return (
+        <>
+            {/* I need to create a new Guest Layout, because <GuestLayout> was originally being used to plop in auth form stuff. GuestLayout is now AuthFormLayout, and a new GuestLayout will be created for things like a homepage or an about page or a contact page that should be identical regardless of user login state. After this, I should be done fixing the things I don’t like about Laravel Breeze. For now, <NavBar /> is just here so I can navigate easily. */}
+            <NavBar />
+            <h1>Hello World</h1>
+        </>
+    );
+};
+
+export default Home;
