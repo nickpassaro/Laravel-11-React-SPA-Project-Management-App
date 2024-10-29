3a43b75455b843884a24abfe65007e0e84fda89e
Nick Passaro <nick.a.passaro@gmail.com>
2024-10-29 16:02:18 -0400

    Added a factory for a single user for the DataBase Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'Nick',
+            'last_name' => 'Passaro',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('1234567890'),
+            'email_verified_at' => time(),
         ]);
     }
 }
