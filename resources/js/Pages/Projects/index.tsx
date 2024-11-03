90233c8b9f1380e39b0e5a78120e6d0c67688a9d
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-03 15:04:58 -0500

    Made Tasks that belong to Projects accessible from displaying Projects

--- resources/js/Pages/Projects/index.tsx
+++ resources/js/Pages/Projects/index.tsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { Project } from "@/types/index";
+import PageSectionCard from "@/Components/PageSectionCard";
 
 interface IndexProps {
     projects: {
@@ -12,49 +13,44 @@ export default function Index({ projects }: IndexProps) {
     return (
         <AuthenticatedLayout header="All Projects">
             <Head title="All Projects" />
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Due Date</th>
-                        <th>Status</th>
-                        <th>Created By</th>
-                        <th>Updated By</th>
-                        <th>Created At</th>
-                        <th>Updated At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {projects.data.map((project) => (
+            <PageSectionCard className="overflow-x-scroll">
+                <table className="whitespace-nowrap">
+                    <thead>
                         <tr>
-                            <td>{project.id}</td>
-                            <td>
-                                <img
-                                    src={project.image_path}
-                                    alt={project.image_path}
-                                    width="50"
-                                    height="50"
-                                />
-                            </td>
-                            <td>{project.name}</td>
-                            <td>{project.due_date}</td>
-                            <td>{project.status}</td>
-                            <td>
-                                {project.created_by.first_name}{" "}
-                                {project.created_by.last_name}
-                            </td>
-                            <td>
-                                {project.updated_by.first_name}{" "}
-                                {project.updated_by.last_name}
-                            </td>
-                            <td>{project.created_at}</td>
-                            <td>{project.updated_at}</td>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Due Date</th>
+                            <th>Status</th>
+                            <th>Tasks</th>
+                            <th>Created By</th>
+                            <th>Updated By</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {projects.data.map((project) => (
+                            <tr key={project.id} className="odd:bg-white even:bg-gray-100 dark:odd:bg-neutral-900 dark:even:bg-neutral-800">
+                                <td className="p-4">{project.id}</td>
+                                <td className="p-4 max-w-96 text-ellipsis overflow-hidden">{project.name}</td>
+                                <td className="p-4">{new Date(project.due_date).toLocaleString()}</td>
+                                <td className="p-4">{project.status}</td>
+                                <td className="p-4">{project.tasks.length}</td>
+                                <td className="p-4">
+                                    {project.created_by.first_name}{" "}
+                                    {project.created_by.last_name}
+                                </td>
+                                <td className="p-4">
+                                    {project.updated_by.first_name}{" "}
+                                    {project.updated_by.last_name}
+                                </td>
+                                <td className="p-4">{new Date(project.created_at).toLocaleString()}</td>
+                                <td className="p-4">{new Date(project.updated_at).toLocaleString()}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </PageSectionCard>
         </AuthenticatedLayout>
     );
 }
