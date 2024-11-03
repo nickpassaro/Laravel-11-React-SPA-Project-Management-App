b3b31937eb380bdab3796bef1f580df5036eefcb
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-03 15:04:18 -0500

    Added flexibility to Page Section Card styling

--- resources/js/Components/PageSectionCard.tsx
+++ resources/js/Components/PageSectionCard.tsx
@@ -1,10 +1,12 @@
 export default function PageSectionCard({
     children,
+    className
 }: {
     children: React.ReactNode;
+    className?: string;
 }) {
     return (
-        <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+        <div className={`bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800 ${className}`}>
             {children}
         </div>
     );
