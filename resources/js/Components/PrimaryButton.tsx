b5962db2bf5fe9782f3d21cd0f66dadc32a00db8
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-04 14:35:00 -0500

    Allowed PrimaryButtons to function as Links without losing their button appearance

--- resources/js/Components/PrimaryButton.tsx
+++ resources/js/Components/PrimaryButton.tsx
@@ -1,21 +1,31 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from "react";
+import { Link } from "@inertiajs/react";
 
 export default function PrimaryButton({
-    className = '',
+    className = "",
     disabled,
     children,
+    link = false,
+    href = "",
     ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
+}: ButtonHTMLAttributes<HTMLButtonElement> & {
+    link?: boolean;
+    href?: string;
+}) {
+    const styles =
+        `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 ${
+            disabled && "opacity-25"
+        } ` + className;
+    if (link) {
+        return (
+            <Link href={href} className={styles} disabled={disabled}>
+                {children}
+            </Link>
+        );
+    }
+
     return (
-        <button
-            {...props}
-            className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
-            disabled={disabled}
-        >
+        <button {...props} className={styles} disabled={disabled}>
             {children}
         </button>
     );
