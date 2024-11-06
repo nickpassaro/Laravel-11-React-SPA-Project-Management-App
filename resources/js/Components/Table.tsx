65368105d577ac22032983c2e0800d18189ed355
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-06 11:32:48 -0500

    Built sorting functionality in the table, leveraging the newly passed in column labels and ids, as well as the passed in sort direction to hande varied defaults when no query strings are present

--- resources/js/Components/Table.tsx
+++ resources/js/Components/Table.tsx
@@ -1,3 +1,5 @@
+import { router } from "@inertiajs/react";
+
 export default function Table({
     columns,
     resourceType,
@@ -9,6 +11,30 @@ export default function Table({
     sortDirection: string;
     children: React.ReactNode;
 }) {
+    // create the query string that till be handed to the router and then run the route
+    const handleSort = (columnKey: string) => {
+        const currentParams = new URLSearchParams(window.location.search);
+
+        // Remove the page parameter to reset pagination
+        currentParams.delete("page");
+
+        // set sortField to column, then invert sortDirection
+        const newSortField = columnKey;
+        let newSortDirection = currentParams.get("sortDirection");
+        if (!newSortDirection) {
+            newSortDirection = sortDirection;
+        }
+        newSortDirection = newSortDirection === "asc" ? "desc" : "asc";
+
+        currentParams.set("sortField", newSortField);
+        currentParams.set("sortDirection", newSortDirection);
+
+        router.get(
+            route(`${resourceType}.index`),
+            Object.fromEntries(currentParams)
+        );
+    };
+
     return (
         <table className="border-collapse border-x-2 border-b-2 border-gray-200 dark:border-gray-800 whitespace-nowrap min-w-full">
             <thead className="bg-slate-200 dark:bg-slate-600 dark:text-gray-300">
