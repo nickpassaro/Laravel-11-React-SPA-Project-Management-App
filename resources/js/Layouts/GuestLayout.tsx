163ff206052748312c6e251ca32f885b34212556
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-07 17:19:10 -0500

    Fixed lack of h1 on pages

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -12,9 +12,9 @@ export default function Guest({
             {header && (
                 <header className="bg-white shadow dark:bg-gray-800">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        <h1 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                             {header}
-                        </h2>
+                        </h1>
                     </div>
                 </header>
             )}
