fe786d0fba21eaffa7add2e78924851cd56ae7e7
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-10 14:10:12 -0500

    Added Create button to project index

--- resources/js/Components/NewPostButton.tsx
+++ resources/js/Components/NewPostButton.tsx
@@ -1,16 +1,14 @@
-import { Link, usePage } from "@inertiajs/react";
+import { Link } from "@inertiajs/react";
 
 export default function NewPostButton({ currentUrl }: { currentUrl: string }) {
-    const url = usePage();
-    const newUrl = `${url.url}/new`;
-
+    const baseUrl = currentUrl.split(".")[0];
     return (
         <Link
-            href={newUrl}
+            href={baseUrl + "/new"}
             className="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150"
         >
             New{" "}
-            {currentUrl
+            {baseUrl
                 .split("/")
                 .map((word) => word.charAt(0).toUpperCase() + word.slice(1, -1))
                 .join(" ")}
--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,32 +1,40 @@
 import AuthNavBar from "@/Components/AuthNavBar";
 import { PropsWithChildren, ReactNode } from "react";
 import NewPostButton from "@/Components/NewPostButton";
-import { usePage } from "@inertiajs/react";
 
 export default function Authenticated({
     header,
     isTablePage = false,
     children,
 }: PropsWithChildren<{ header?: ReactNode; isTablePage?: boolean }>) {
-    const { url } = usePage();
     return (
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
             <AuthNavBar />
 
             {header && (
                 <header className="bg-white shadow dark:bg-gray-800">
-                    <div className="flex justify-between mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <div className="flex justify-between items-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                         <h1 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                             {header}
                         </h1>
-                        {url.startsWith('/projects') && <NewPostButton currentUrl={url.slice(1)} />}
+                        {route().current()?.includes("index") && route().current()?.includes("projects") && (
+                            <NewPostButton
+                                currentUrl={route().current() || ""}
+                            />
+                        )}
                     </div>
                 </header>
             )}
 
             <main>
                 <div className="py-12">
-                    <div className={`mx-auto ${isTablePage === false ? 'max-w-7xl sm:px-6 lg:px-8 space-y-6' : ''}`}>
+                    <div
+                        className={`mx-auto ${
+                            isTablePage === false
+                                ? "max-w-7xl sm:px-6 lg:px-8 space-y-6"
+                                : ""
+                        }`}
+                    >
                         {children}
                     </div>
                 </div>
