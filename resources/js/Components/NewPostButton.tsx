e7655fdaf0b2c3c058dc9a86845a5dbce315d09f
Nick Passaro <nick.a.passaro@gmail.com>
2024-11-10 14:18:01 -0500

    Fixed new post url

--- resources/js/Components/NewPostButton.tsx
+++ resources/js/Components/NewPostButton.tsx
@@ -4,7 +4,7 @@ export default function NewPostButton({ currentUrl }: { currentUrl: string }) {
     const baseUrl = currentUrl.split(".")[0];
     return (
         <Link
-            href={baseUrl + "/new"}
+            href={baseUrl + "/create"}
             className="inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-900 focus:outline-none focus:border-blue-900 focus:ring ring-blue-300 disabled:opacity-25 transition ease-in-out duration-150"
         >
             New{" "}
